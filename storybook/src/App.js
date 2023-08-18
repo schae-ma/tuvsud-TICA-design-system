1aa28cfef3cb50d51b95883044b107b9c3cff0b6
Viúla Pires <pires-ca@de001.itgr.net>
2023-08-18 09:02:25 +0200

    add storybook

---
+++ storybook/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Accordion from './components/Accordion';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Accordion/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ storybook/src/components/Accordion.jsx
@@ -0,0 +1,39 @@
+import AccordionItem from "./AccordionItem"
+import PropTypes from 'prop-types';
+import "./accordion.css"
+const Accordion = ({size, backgroundColor})=>{
+    
+    return (
+        <>  
+            <AccordionItem label={"Accordion 1"} size={size} backgroundColor={backgroundColor}>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+                </p>
+            </AccordionItem>
+            <AccordionItem label={"Accordion 2"} size={size} backgroundColor={backgroundColor}>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat.
+                </p>
+            </AccordionItem>
+
+        </>
+    )
+}
+
+export default Accordion
+
+Accordion.propTypes = {
+    backgroundColor: PropTypes.string,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    
+  };
+  
+  Accordion.defaultProps = {
+    size: 'medium',
+  };
