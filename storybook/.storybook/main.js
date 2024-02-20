316c6ac614d4d018a29801e1d5a370bc1c6b1b44
Viúla Pires <catarina.vi.pires@gmail.com>
2024-02-20 11:11:22 +0100

    accordion scss

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -7,6 +7,8 @@ const config = {
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@chromaui/addon-visual-tests",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/react-webpack5",
